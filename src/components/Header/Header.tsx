81204207db0c06713f02e71876e51d8104fd5411
Kacper Drewniak <k.drewniak.dev@gmail.com>
2023-11-04 16:06:38 +0100

    fix styled component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: red;
+`;
 
 const Header = () => {
   return (
     <div>
       <Navbar />
-      header
+      <Container>header</Container>
     </div>
   );
 };
